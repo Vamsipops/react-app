c608bd605a2064c5cbbc33fb8cab3a3e5e290eb3
Vamsipops <vamsipopuri1997@gmail.com>
2025-04-11 11:35:56 -0400

    first react app

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,15 @@
+
+function Card(){
+
+    return(
+        <div className = "card">
+        <img src="https://via.placeholder.com/200" alt="picture of 200*200"></img>
+        <h2>Card Title</h2>
+        <p>This is a simple card component.</p>
+        </div>
+    );
+
+    };
+
+
+export default Card;
---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,17 @@
+
+function Food(){
+    const food1 = 'Pizza';
+    const food2 = 'Burger';
+
+    return(
+        <ul>
+            <li>{food1}</li>
+            <li>{food2}</li>
+            <li>Ice Cream</li>
+            <li>Fries</li>
+        </ul>
+    )
+
+}
+
+export default Food;
